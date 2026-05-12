69ef44aecc3eeade91b2f85fd9909d7476765936
unknown <chippochoppo87@hotmail.com>
2026-05-12 14:56:56 +0200

    Enable PWA support for Rödpäls-Resan

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [
+    react(), 
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'icons.svg'],
+      manifest: {
+        name: 'Rödpäls-Resan',
+        short_name: 'Rödpäls',
+        description: 'Guide för Finsk Spets',
+        theme_color: '#c04e22',
+        icons: [
+          {
+            src: 'icons.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'icons.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ],
 })
